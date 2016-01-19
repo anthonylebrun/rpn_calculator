2c710a54d6d2f733697edd4da87e272f1045105f
Pierre Lebrun <anthonylebrun@gmail.com>
2016-01-19 14:44:14 -0500

    Updated path reference

--- lib/language.rb
+++ lib/language.rb
@@ -3,7 +3,7 @@ require 'json'
 require 'ostruct'
 
 # Load the yaml file into a hash
-language_hash = YAML.load_file(File.join(File.dirname(__FILE__), '../data/language.yml'))
+language_hash = YAML.load_file(File.join(File.dirname(__FILE__), '../config/language.yml'))
 
 # Convert the nested hash to nested openstruct and freeze the final object
 $language = JSON.parse(language_hash.to_json, object_class: OpenStruct).freeze
